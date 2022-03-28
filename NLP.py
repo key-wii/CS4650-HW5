ee01ef2f9710973b4f7e3fbb33026c615be3ff4e
Christopher <programmerchristopher@gmail.com>
2022-03-27 23:50:31 -0700

    output to txt

--- NLP.py
+++ NLP.py
@@ -3,33 +3,59 @@ import spacy
 from newsapi import NewsApiClient
 import pickle
 import pandas as pd
+from collections import Counter
+from string import punctuation
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 nlp_eng = spacy.load('en_core_web_lg')
 newsapi = NewsApiClient (api_key='02f0ae732bcb4f2db1d77ef4288f237e')
 
-data = newsapi.get_everything(q='coronavirus', language='en',
-    from_param='2022-02-27', to='2022-03-27', sort_by='relevancy', page_size = 100)
+def data(x):
+    temp = newsapi.get_everything(q='coronavirus', language='en',
+        from_param='2022-02-27', to='2022-03-27',
+        sort_by='relevancy', page=x)
+    return temp
 
-print("total result: " + str(data['totalResults']))
-
-dados = {}
-titles = []
-dates = []
-descriptions = []
-articles = data['articles']
+dados = []
+articles = list(map(data, range(1, 6)))
+print(articles)
 
 filename = 'articlesCOVID.pckl'
 pickle.dump(articles, open(filename, 'wb'))
-text_file = open('result.txt')
 
 for i, article in enumerate(articles):
     for x in article['articles']:
         title = x['title']
         description = x['description']
         content = x['content']
-        dados.append({'title':titles[0], 'date':dates[0], 'desc':descriptions[0], 'content':content})
-        text_file.write("information from the API call")
+        dados.append({'title': title, 'desc': description, 'content': content})
 df = pd.DataFrame(dados)
 df = df.dropna()
 df.head()
 
+def get_keywords_eng(text):
+    result = []
+    pos_tag = ['PROPN', 'VERB', 'NOUN']
+    doc = nlp_eng(text)
+    for token in doc:
+        if (token.text in nlp_eng.Defaults.stop_words or token.text in punctuation):
+            continue
+        if (token.pos_ in pos_tag):
+            result.append(token.text)
+    return result
+
+results = []
+for content in df.content.values:
+    results.append([('#' + x[0]) for x in Counter(get_keywords_eng(content)).most_common(5)])
+df['keywords'] = results
+
+text = str(results)
+text_file = open('result.txt', 'w')
+text_file.write(text)
+
+wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="black").generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
+plt.axis("off")
+plt.show()
